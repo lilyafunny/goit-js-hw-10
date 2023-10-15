e9bbf31a0316cc939652c5a8f9a2b086e3ef36ac
lilyafunny <125149545+lilyafunny@users.noreply.github.com>
2023-10-15 12:02:20 +0300

    sum

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 // live_KVsXT7CLTuuUbwPYhNdiAHy7V4irrxcZVvCgh3F6Kdt1aGz5En2ssKxN9nSo3PXb // API-KAY
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_KVsXT7CLTuuUbwPYhNdiAHy7V4irrxcZVvCgh3F6Kdt1aGz5En2ssKxN9nSo3PXb';
+
+const url = `https://api.thecatapi.com/v1/breeds`;
